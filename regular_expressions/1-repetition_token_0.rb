554ab9daeb270593c564bee7ebd05c44a016cbe9
codestantcaesar <c.akas@alustudent.com>
2025-01-13 17:46:02 -0800

    add this file

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -3,8 +3,8 @@
 # Accept the first argument from the command line
 input = ARGV[0]
 
-# Regular expression to match the pattern with up to 6 't's
-regex = /^hbt{1,6}n$/
+# Regular expression to match the pattern with 1 to 6 't's
+regex = /^hbt{1,5}n$/
 
 # Check if the input matches the pattern and print the result
 if input.match(regex)
