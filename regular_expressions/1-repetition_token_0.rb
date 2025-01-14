037355d8b10ccdd5acb388d996b25e701aa9e8bd
codestantcaesar <c.akas@alustudent.com>
2025-01-13 16:46:07 -0800

    add this files

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/env ruby
 
 
-def match_pattern(input)  
-  # Regular expression to match the specified patterns  
-  regex = /^hbt+n$/  
-  if input.match(regex)  
-    puts "Match"  
-  else  
-    puts "No Match"  
-  end  
-end  
+#!/usr/bin/env ruby
 
-# Get the input from command line arguments  
-if ARGV.length != 1  
-  puts "Please provide exactly one argument."  
-else  
-  match_pattern(ARGV[0])  
+# Check if an argument is passed
+if ARGV.length != 1
+  puts "Usage: ./match_pattern.rb <string>"
+  exit 1
+end
+
+# Input string
+input = ARGV[0]
+
+# Regular expression to match the pattern
+regex = /^hb(tt*)n$/
+
+# Check if the input matches the pattern
+if input.match?(regex)
+  puts input
 end
